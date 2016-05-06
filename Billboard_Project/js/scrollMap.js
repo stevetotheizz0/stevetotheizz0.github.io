66cd334c1914c9acf96751322751680de2adddf9
stevetotheizz0 <stephenjskilton@gmail.com>
2016-05-06 05:01:15 -0400

    fulldataset

--- Billboard_Project/js/scrollMap.js
+++ Billboard_Project/js/scrollMap.js
@@ -2,36 +2,94 @@
 var narrative = document.getElementById('narrative'),
     sections = narrative.getElementsByTagName('section'),
     currentId = 'first';
-var currentLayer;
+
+/*
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+    billboards = L.geoJson(rawBillboard, {
+            pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);}
+    }).addTo(map);
+    currentLayer = billboards;
+*/
 
 
 var locations = function(){
   map.removeLayer(currentLayer);
-  map.addLayer(billboards);
+  var geojsonMarkerOptions = function(feature) {
+        switch (feature.properties.Digital) {
+          case 'no': return {fillColor: "#000000", radius:1, fillOpacity: 1, stroke:false};
+          case 'yes':   return {fillColor: "#b2182b", radius:4, fillOpacity: 1, stroke:false};
+        }
+      };
+  billboards = L.geoJson(rawBillboard, {
+    pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng, geojsonMarkerOptions(feature));}
+  }).addTo(map);
   currentLayer = billboards;
 };
 var traffic = function(){
   map.removeLayer(currentLayer);
-  map.addLayer(billboards);
+  var geojsonMarkerOptions = function(feature) {
+        if(feature.properties.DLY_VMT < 10000) {                                             return {fillColor: "#D79E9E", radius:2, fillOpacity: 1, stroke:true, weight: 0.5, opacity: 1,color: "#000000", };}
+        else if(feature.properties.DLY_VMT > 10000 && feature.properties.DLY_VMT <= 25000) { return {fillColor: "#D76666", radius:3, fillOpacity: 1, stroke:true, weight: 0.5, opacity: 1,color: "#000000"};}
+        else if(feature.properties.DLY_VMT > 25000 && feature.properties.DLY_VMT <= 50000) { return {fillColor: "#F5A27A", radius:4, fillOpacity: 1, stroke:true, weight: 0.5, opacity: 1,color: "#000000"};}
+        else if(feature.properties.DLY_VMT > 50000 && feature.properties.DLY_VMT <= 100000) {return {fillColor: "#C80000", radius:5, fillOpacity: 1, stroke:true, weight: 0.5, opacity: 1,color: "#000000"};}
+        else {                                                                               return {fillColor: "#940000", radius:6, fillOpacity: 1, stroke:true, weight: 0.5, opacity: 1,color: "#000000"};}
+      };
+  billboards = L.geoJson(rawBillboard, {
+    pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng, geojsonMarkerOptions(feature));}
+  }).addTo(map);
   currentLayer = billboards;
 };
 var buildings = function(){
   map.removeLayer(currentLayer);
-  map.addLayer(billboards);
+  var geojsonMarkerOptions = function(feature) {
+        switch (feature.properties.building) {
+          case 'Building':                  return {fillColor: "#A80000", radius:5, fillOpacity: 1, stroke:false};
+          case 'Building, Near Match':      return {fillColor: "#FFA77F", radius:3, fillOpacity: 1, stroke:false};
+          case 'No Building':               return {color: "#000000", radius:3, fillOpacity: 0, stroke:true, weight: 1, opacity: 1};
+          case 'No Building, Near Match':   return {color: "#000000", radius:2, fillOpacity: 0, stroke:true, weight: 1, opacity:1};
+          case 'Unmatched':                 return {fillColor: "#828282", radius:2, fillOpacity: 1, stroke: false};
+        }
+      };
+  billboards = L.geoJson(rawBillboard, {
+    pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng, geojsonMarkerOptions(feature));}
+  }).addTo(map);
   currentLayer = billboards;
 };
 var digital = function(){
   map.removeLayer(currentLayer);
-  map.addLayer(billboards);
+  var geojsonMarkerOptions = function(feature) {
+        switch (feature.properties.building) {
+          case 'Building':                  return {fillColor: "#A80000", radius:5, fillOpacity: 1, stroke:false};
+          case 'Building, Near Match':      return {fillColor: "#FFA77F", radius:3, fillOpacity: 1, stroke:false};
+          case 'No Building':               return {color: "#000000", radius:3, fillOpacity: 0, stroke:true, weight: 1, opacity: 1};
+          case 'No Building, Near Match':   return {color: "#000000", radius:2, fillOpacity: 0, stroke:true, weight: 1, opacity:1};
+          case 'Unmatched':                 return {fillColor: "#828282", radius:2, fillOpacity: 1, stroke: false};
+        }
+      };
+  billboards = L.geoJson(rawBillboard, {
+    pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng, geojsonMarkerOptions(feature));}
+  }).addTo(map);
   currentLayer = billboards;
 };
 
 var changeMap = function(pageSection){
   switch (pageSection) {
-      case 'first': locations(); break;
-      case 'second': traffic();  break;
-      case 'third': buildings(); break;
-      case 'fourth': digital(); break;
+    case 'first': locations(); break;
+    case 'second': traffic();  break;
+    case 'third': buildings(); break;
+    case 'fourth': digital();
   }
 };
 
--- Billboard_Project/js/setup.js
+++ Billboard_Project/js/setup.js
@@ -1,6 +1,6 @@
 var billboards;
     philaBounds = "http://data.phl.opendata.arcgis.com/datasets/405ec3da942d4e20869d4e1449a2be48_0.geojson";
-    dataset ="https://raw.githubusercontent.com/stevetotheizz0/stevetotheizz0.github.io/master/Billboard_Project/json/billboardData.geojson";
+    dataset ="https://raw.githubusercontent.com/stevetotheizz0/stevetotheizz0.github.io/master/Billboard_Project/json/billboardData.json";
     parks = "Billboard_Project/json/PhiladelphiaParks.geojson";
     boundaryStyle = {
     "color": "#252525",
@@ -27,11 +27,27 @@ var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/ton
   ext: 'png'
 }).addTo(map);
 
+var rawBillboard;
+var currentLayer;
+
+var geojsonMarkerOptions = function(feature) {
+      switch (feature.properties.Digital) {
+          case 'no': return {fillColor: "#000000", radius:2, fillOpacity: 1, stroke:false};
+          case 'yes':   return {fillColor: "#b2182b", radius:4, fillOpacity: 1, stroke:false};
+      }
+    };
+
+
 $(document).ready(function() {
   $.ajax(philaBounds).done(function(data) {
     myFeatureGroup = L.geoJson(data, {"style":boundaryStyle}).addTo(map);
   });
   $.ajax({url: dataset, dataType: 'json'}).done(function(data) {
-    billboards = L.geoJson(data).addTo(map);
+    rawBillboard = data;
+    billboards = L.geoJson(rawBillboard, {
+            pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions(feature));}
+    }).addTo(map);
+    currentLayer = billboards;
   });
 });
