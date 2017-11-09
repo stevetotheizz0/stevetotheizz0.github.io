3c5b20f638ef9c58c518a8c0cd097e9859872563
stevetotheizz0 <stephenjskilton@gmail.com>
2017-11-08 23:34:27 -0500

    Added Promises Test to Mumbai Project

--- Mumbai_Project/js/setup.js
+++ Mumbai_Project/js/setup.js
@@ -1,4 +1,5 @@
-var dataset = "https://raw.githubusercontent.com/stevetotheizz0/stevetotheizz0.github.io/master/Mumbai_Project/Mumba_Study_Area.json";
+var dataset1 = "https://raw.githubusercontent.com/stevetotheizz0/stevetotheizz0.github.io/master/Mumbai_Project/Mumba_Study_Area.json";
+    dataset2 = "https://raw.githubusercontent.com/stevetotheizz0/stevetotheizz0.github.io/master/Mumbai_Project/Mumbai_Station_Buffer1k.json";
 
 var map = L.map('map', {
   center: [18.950638, 72.837817],
@@ -14,9 +15,32 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{
   ext: 'png'
 }).addTo(map);
 
-  $(document).ready(function() {
-    $.ajax(dataset).done(function(data) {
-      parsedData = JSON.parse(data);
-      myFeatureGroup = L.geoJson(parsedData).addTo(map);
+
+$(document).ready(function () {
+    var j1 = $.ajax({
+        url: dataset1,
+        dataType: 'json',
+        type: 'GET'
     });
-  });
+
+    var j2 = $.ajax({
+        url: dataset2,
+        dataType: 'json',
+        type: 'GET'
+    });
+
+    $.when(j1, j2).then(function(data1, data2) {
+      myFeatureGroup1 = L.geoJson(data1).addTo(map);
+      myFeatureGroup2 = L.geoJson(data2).addTo(map);
+    }, function (jqXHR, textStatus, errorThrown) {
+        var x1 = j1;
+        var x2 = j2;
+        if (x1.readyState != 4) {
+            x1.abort();
+        }
+        if (x2.readyState != 4) {
+            x2.abort();
+        }
+       alert('Either j1 or j2 failed!');
+    });
+});
