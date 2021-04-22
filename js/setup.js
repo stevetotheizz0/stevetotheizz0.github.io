4a3596a763a35e56b0451fc70573544c64611df6
stevetotheizz0 <stephenjskilton@gmail.com>
2021-04-22 09:49:39 -0400

    https

--- js/setup.js
+++ js/setup.js
@@ -5,8 +5,8 @@ var map = L.map('map', {
   scrollWheelZoom: false,
 });
 
-var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',{
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+var Stamen_TonerLite = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',{
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
   subdomains: 'abcd',
   minZoom: 0,
   maxZoom: 20,
